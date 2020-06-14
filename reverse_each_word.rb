473f57841623a66c44410605c1be4153f8d4ad82
Derek Turner <turner.derek.b@gmail.com>
2020-06-14 20:16:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 def reverse_each_word(sentence)
   array_to_reverse = sentence.split(/ /)
   array_to_reverse.map do |string|
-    string.reverse.join
+    string.reverse
+    string.join
   end
   
 end
